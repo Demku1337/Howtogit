325e30b1ecdd82f113ab792653fb2a13a703cfc1
Demku1337 <demku.roland@gmail.com>
2017-11-17 15:51:20 +0100

    Fizz kiírja a számokat 1-100-ig

---
+++ Fizz/src/Fizz.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Roland on 2017.11.17..
+ */
+public class Fizz {
+    public static void main(String[] args){
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
