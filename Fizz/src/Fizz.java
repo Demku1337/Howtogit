4b50f3a7758e5f58366e5ec918d2bc5078aec2d7
Demku1337 <demku.roland@gmail.com>
2017-11-17 16:43:46 +0100

    Minden 3-mal osztható szám után kiírja, hogy Fizz

--- Fizz/src/Fizz.java
+++ Fizz/src/Fizz.java
@@ -2,9 +2,16 @@
  * Created by Roland on 2017.11.17..
  */
 public class Fizz {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         for (int i = 1; i <= 100; i++) {
-            System.out.println(i);
+           System.out.println(i);
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+
+            }
+
+
         }
     }
 }
