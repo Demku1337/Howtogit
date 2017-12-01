1d852916a39c52763c1ff1472a0e28f622e70f8a
Demku1337 <demku.roland@gmail.com>
2017-12-01 14:36:01 +0100

    Ez a Fizzben a Buzz

---
+++ Fizz/src/Buzz.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Roland on 2017.12.01..
+ */
+public class Buzz {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+            if (i % 4 == 0) {
+                System.out.println("Buzz");
+
+            }
+
+
+        }
+    }
+}
